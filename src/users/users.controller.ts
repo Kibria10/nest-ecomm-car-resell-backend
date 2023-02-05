0535777ae88ee448762f2f14ffd38d9bd4167802
Maharab Kibria <junakkibria56@gmail.com>
2023-02-05 11:18:32 +0600

    10.3 implementing basic interceptor logic

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,9 @@
-import { Body, Controller, Delete, Get, NotFoundException, Param, Patch, Post, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, NotFoundException, Param, Patch, Post, Query, UseInterceptors } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
+import { SerializeInterceptor } from 'src/interceptors/serialize.interceptor';
+
 @Controller('auth')
 export class UsersController {   
     constructor(private usersService: UsersService){}
@@ -12,8 +14,10 @@ export class UsersController {
         this.usersService.create(body.email, body.password);
     }
 
+    @UseInterceptors(SerializeInterceptor)
     @Get('/:id')
     async findUser(@Param('id') id: string){
+        console.log('handler is running');
         const user = await this.usersService.findOne(parseInt(id));
         if(!user){
             throw new NotFoundException('User not found');
