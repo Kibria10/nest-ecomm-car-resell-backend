5bb27a2968df1ddbf82fef7a79dcb091dc230875
Maharab Kibria <junakkibria56@gmail.com>
2023-02-06 17:31:27 +0600

    11.16 current-user interceptor setup

--- src/users/decorators/current-user.decorator.ts
+++ src/users/decorators/current-user.decorator.ts
@@ -7,6 +7,6 @@ export const CurrentUser = createParamDecorator(
     //the never type is to ensure that CurrentUser decorator does not need any data passed to it
     (data: never, context: ExecutionContext) => {
     const request = context.switchToHttp().getRequest();
-    return request.session.userId;
+    return request.CurrentUser;
     }
 )
