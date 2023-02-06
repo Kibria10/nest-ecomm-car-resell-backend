6d245d9cc54a8efaa9dbd5faf7a270cdbaa65bc7
Maharab Kibria <junakkibria56@gmail.com>
2023-02-06 15:55:03 +0600

    11.8 cookie-session installation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+const cookieSession = require('cookie-session'); //old way of importing modules
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieSession({
+    keys: ['asdfasdf'] //this is the encryption key for the cookie
+  }));
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true, //removes extra property if users tries to send extra keys with theier incoming body request. basically allows to send only the keys that are defined in the DTO.
   }));
