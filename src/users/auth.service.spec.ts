daa9e9582dfcd0cbbe778d3c2bb981bae223ce3d
Maharab Kibria <junakkibria56@gmail.com>
2023-03-05 12:42:23 +0600

    12.6 implementation of standard layout
    implemented beforeEach and describe function like every other spec files. describe only shows the name of the test in terminal for a better understanding and beforeEach makes sure the code block runs everytime an iteration happes. to implement beforeEach we created a global service variable to use it inside it function as the it function is in a different scope

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -3,26 +3,32 @@ import { AuthService } from "./auth.service";
 import { User } from "./user.entity";
 import { UsersService } from "./users.service";
 
-//create temporary testing DI container. to load up some classes and some fake implementations of those classes.
-it('can create an instance of auth service', async () => {
-    //create a fake copy of the users service
-    const fakeUsersService : Partial<UsersService> = {
-        find: () => Promise.resolve([]),
-        create: (email: string, password: string) =>
-            Promise.resolve({ id: 1, email, password } as User)
-    }
+describe('AuthService', () => {
+    let service: AuthService;
 
-    //create a module or a DI container    
-    const module = await Test.createTestingModule({
-        providers: [AuthService, {
-            provide: UsersService,
-            useValue: fakeUsersService
-        }],
+    beforeEach(async () => {
+        //create a fake copy of the users service
+        const fakeUsersService: Partial<UsersService> = {
+            find: () => Promise.resolve([]),
+            create: (email: string, password: string) =>
+                Promise.resolve({ id: 1, email, password } as User)
+        }
+        //create a module or a DI container    
+        const module = await Test.createTestingModule({
+            providers: [AuthService, {
+                provide: UsersService,
+                useValue: fakeUsersService
+            }],
 
-    }).compile();
+        }).compile();
 
-    //get an instance of the auth service from the DI container
-    const service = module.get<AuthService>(AuthService);
-    //assert that the service is defined
-    expect(service).toBeDefined();
+        //get an instance of the auth service from the DI container
+        service = module.get<AuthService>(AuthService);
+    });
+
+    //create temporary testing DI container. to load up some classes and some fake implementations of those classes.
+    it('can create an instance of auth service', async () => {
+        //assert that the service is defined
+        expect(service).toBeDefined();
+    });
 });
