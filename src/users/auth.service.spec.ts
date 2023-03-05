5324f3e7f7df9b2071c156c635bced0f2bc4ebc2
Maharab Kibria <junakkibria56@gmail.com>
2023-03-05 12:10:50 +0600

    12.2 completion of unit test with success

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -3,14 +3,25 @@ import { AuthService } from "./auth.service";
 import { UsersService } from "./users.service";
 
 //create temporary testing DI container. to load up some classes and some fake implementations of those classes.
-it('can create an instance of auth service', async () => { 
+it('can create an instance of auth service', async () => {
+    //create a fake copy of the users service
+    const fakeUsersService = {
+        find: () => Promise.resolve([]),
+        create: (email: string, password: string) =>
+            Promise.resolve({ id: 1, email, password })
+    }
+
     //create a module or a DI container    
     const module = await Test.createTestingModule({
-        providers: [AuthService]
+        providers: [AuthService, {
+            provide: UsersService,
+            useValue: fakeUsersService
+        }],
+
     }).compile();
 
-//get an instance of the auth service from the DI container
+    //get an instance of the auth service from the DI container
     const service = module.get<AuthService>(AuthService);
-//assert that the service is defined
+    //assert that the service is defined
     expect(service).toBeDefined();
 });
