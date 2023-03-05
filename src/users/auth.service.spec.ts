73e08e2300421f4aa8cff685b9be3c6f2bdecfe6
Maharab Kibria <junakkibria56@gmail.com>
2023-03-05 12:32:06 +0600

    12.4 type casting the fakeUsersService

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -1,14 +1,15 @@
 import { Test } from "@nestjs/testing";
 import { AuthService } from "./auth.service";
+import { User } from "./user.entity";
 import { UsersService } from "./users.service";
 
 //create temporary testing DI container. to load up some classes and some fake implementations of those classes.
 it('can create an instance of auth service', async () => {
     //create a fake copy of the users service
-    const fakeUsersService = {
+    const fakeUsersService : Partial<UsersService> = {
         find: () => Promise.resolve([]),
         create: (email: string, password: string) =>
-            Promise.resolve({ id: 1, email, password })
+            Promise.resolve({ id: 1, email, password } as User)
     }
 
     //create a module or a DI container    
