512070676bc1414c6cf5900813c029e04eeb4634
Maharab Kibria <junakkibria56@gmail.com>
2023-03-06 14:26:45 +0600

    12.16 test case for signin route handler

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -26,7 +26,9 @@ describe('UsersController', () => {
     };
     fakeAuthService = {
       // signup: () => {},
-      // signin: () => {},
+      signin: (email: string, password: string) => {
+        return Promise.resolve({ id: 1, email, password } as User)
+      },
     };
 
     const module: TestingModule = await Test.createTestingModule({
@@ -64,6 +66,19 @@ describe('UsersController', () => {
   it('throws an error if user is not found', async () => {
     fakeUsersService.findOne = () => null;
     await expect(controller.findUser('1')).rejects.toThrow();
+  });
+
+  it('signin updates session object and returns user', async () => {
+    const session = { userId: -10 };
+    const user = await controller.signin(
+      {
+        email: 'asdf@asdf.com',
+        password: 'asdf',
+      },
+      session,
+    );
+    expect(user.id).toEqual(1);
+    expect(session.userId).toEqual(1);
   });
 
 });
