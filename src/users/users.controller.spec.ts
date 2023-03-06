4a69e35e1b54a4f8425e79e7b42ce2e0fcc03ec2
Maharab Kibria <junakkibria56@gmail.com>
2023-03-06 12:07:24 +0600

    12.13 unit testing users controller scratch setup
    in unit testing of a controller we do not have to worry about the decorators, we just need to ensure if the route handlers are working properly or not. to set up the test file, we need to check which services are the route handlers using then set up a mock/fake service for these handlers inside of the spec file and define the methods

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,10 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { AuthService } from './auth.service';
+import { User } from './user.entity';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let fakeUsersService: Partial<UsersService>;
+  let fakeAuthService: Partial<AuthService>;
 
   beforeEach(async () => {
+    fakeUsersService = {
+      find: () => {},
+      create: () => {},
+      findOne: () => {},
+      remove: () => {},
+      uodate: () => {},
+    };
+    
+    fakeAuthService = {
+      signup: () => {},
+      signin: () => {},
+    };
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
     }).compile();
